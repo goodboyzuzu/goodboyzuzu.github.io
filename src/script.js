3086bf140723e73366640a2aeb525bf236a91cf4
goodboyzuzu <gohzuhong@gmail.com>
2022-01-16 22:59:14 +0800

    animation for intro done

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-gsap.from('.navigation', {duration: 1,y:'-100%'})
+gsap.from('.navigation', {duration: 1,y:'-100%'});
+gsap.from('.redfrog',{duration:1,y:'-100%', x:'100%'})
+gsap.from('.smokefrog',{duration:1,y:'100%', x:'100%'})
+gsap.from('.rocketfrog',{duration:1,y:'-100%', x:'-100%'})
+gsap.from('.bluefrog',{duration:1,y:'100%', x:'-100%'})
+gsap.from('.bigfrog',{duration:0.5,y:'100%'})
